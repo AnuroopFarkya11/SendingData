24ba6409d5dccf1b0ccfe4cf498a29c80922145b
Anuroop Farkya <anuroopf02@gmail.com>
2022-12-24 10:39:24 +0530

    Initial Commit

--- app/src/main/java/com/example/sendingdata/MainActivity.java
+++ app/src/main/java/com/example/sendingdata/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         names.add("ramji");
         names.add("Sitaji!");
         names.add("SHiv bhgwan");
+        names.add("Namah Shivaaye");
 
 
 
